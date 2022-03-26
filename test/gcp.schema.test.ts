cf0956f537d3e0db1e8d063cc565520f10513845
Aarne Laur <aarne.laur@gmail.com>
2022-03-26 12:26:16 +0100

    feat: Typings for AlertData and TestMatrixEventData

--- test/gcp.schema.test.ts
+++ test/gcp.schema.test.ts
@@ -4,5 +4,5 @@ import { source } from '../tools/generate'
 
 test('GCP schema has not changed since last build', async () => {
     const { data } = await axios.get(source)
-    expect(hash(data)).toBe('2934e6a84d0bb93a5e10313b053ff93f2b83c017')
+    expect(hash(data)).toBe('aeb8316da57b02cb73f7381b088fdf72b9c9154c')
 })
