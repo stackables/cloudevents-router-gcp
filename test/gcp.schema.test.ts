c658e6911029e62a473e18e86a2c61e247d0d6e7
Aarne Laur <aarne.laur@gmail.com>
2022-11-12 11:12:43 +0100

    feat: Migrate to cloudevents-router@2.0.0

--- test/gcp.schema.test.ts
+++ test/gcp.schema.test.ts
@@ -4,5 +4,5 @@ import { source } from '../tools/generate'
 
 test('GCP schema has not changed since last build', async () => {
     const { data } = await axios.get(source)
-    expect(hash(data)).toBe('2934e6a84d0bb93a5e10313b053ff93f2b83c017')
+    expect(hash(data)).toBe('de05661b0e8127ac497b26485b8a13cdafc3c69b')
 })
