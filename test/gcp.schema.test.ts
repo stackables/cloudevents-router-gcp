1b134bb7b0e3e4149716aa168f8cc20dadf1de41
Aarne Laur <aarne.laur@gmail.com>
2024-02-04 13:03:27 +0100

    chore: Node 20 (#81)
    * chore: Node 20
    
    * chore: Add jest as dev dependency

--- test/gcp.schema.test.ts
+++ test/gcp.schema.test.ts
@@ -1,8 +1,8 @@
-import axios from 'axios'
-import hash from 'object-hash'
-import { source } from '../tools/generate'
+import axios from "axios";
+import hash from "object-hash";
+import { source } from "../tools/generate";
 
-test('GCP schema has not changed since last build', async () => {
-    const { data } = await axios.get(source)
-    expect(hash(data)).toBe('87b4e944d85c8502c904513c15cd7095ddb87e4e')
-})
+test("GCP schema has not changed since last build", async () => {
+	const { data } = await axios.get(source);
+	expect(hash(data)).toBe("b6fb2f83ab3539c2c5d888e6dcfbbfbc84455a8f");
+});
